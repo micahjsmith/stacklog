3356fc206ebbacf711e1045ec42d2788f7135401
Micah Smith <micahjsmith@gmail.com>
2020-03-19 14:33:36 -0400

    Bump version: 1.0.0 → 1.1.0

--- setup.py
+++ setup.py
@@ -85,6 +85,6 @@ setup(
     test_suite='tests',
     tests_require=tests_require,
     url='https://github.com/micahjsmith/stacklog',
-    version='1.0.0',
+    version='1.1.0',
     zip_safe=False,
 )
--- stacklog/__init__.py
+++ stacklog/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = 'Micah Smith'
 __email__ = 'micahjsmith@gmail.com'
-__version__ = '1.0.0'
+__version__ = '1.1.0'
 
 import time
 from collections import defaultdict
